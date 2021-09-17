13169305e6f2042d196c7b36d92a6416fe0da5d9
Miguel Albernaz <albernazmiguel@gmail.com>
2021-09-17 18:57:07 -0300

    update fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,4 +1,19 @@
 function fish_prompt --description 'Write out the prompt'
+  set_color $fish_color_autosuggestion 2> /dev/null; or set_color 555
+  if [ $fish_key_bindings = fish_vi_key_bindings ]
+    switch $fish_bind_mode
+      case default
+        echo -n "(n) "
+      case insert
+        echo -n "(i) "
+      case visual
+        echo -n "(v) "
+      case replace-one
+        echo -n "(r) "
+    end
+  end
+
+  set_color white
   echo -n "λ" (prompt_pwd)/
 
   set -l git_branch (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
