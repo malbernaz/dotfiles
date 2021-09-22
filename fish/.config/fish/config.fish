4ae8c63aa0126553cfc4692f80e5ebf839855ef1
Miguel Albernaz <albernazmiguel@gmail.com>
2021-09-22 09:52:22 -0300

    using fisher

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,3 +4,15 @@ end
 
 thefuck --alias | source
 
+# Editor
+set EDITOR 'nvim'
+
+# Aliases
+set -l git_branch (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+alias gpo="git push -u origin $git_branch"
+alias v=nvim
+
