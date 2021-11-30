892dbfaf566eedbee916b6b0aaa77869df97a32e
Miguel Albernaz <albernazmiguel@gmail.com>
2021-11-30 19:36:35 -0300

    better tmux

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -7,9 +7,9 @@ utils.map("n", "`p", '"0p')
 utils.map("n", "`P", '"0P')
 
 -- telescope
-utils.map("n", "<c-p>", "<cmd>lua require('telescope.builtin').git_files({ hidden = true })<cr>")
-utils.map("n", "<c-g>", "<cmd>lua require('telescope.builtin').live_grep({ hidden = true })<cr>")
-utils.map("n", "<c-b>", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+utils.map("n", "<c-p>", ":lua require('telescope.builtin').git_files({ hidden = true })<cr>")
+utils.map("n", "<c-g>", ":lua require('telescope.builtin').live_grep({ hidden = true })<cr>")
+utils.map("n", "<c-b>", ":lua require('telescope.builtin').buffers()<cr>")
 
 -- splits
 utils.map("n", "<c-h>", "<c-w>h")
@@ -17,12 +17,15 @@ utils.map("n", "<c-j>", "<c-w>j")
 utils.map("n", "<c-k>", "<c-w>k")
 utils.map("n", "<c-l>", "<c-w>l")
 
+-- resize splits
+utils.map("n", "<leader>s", ":resize +5<cr>")
+utils.map("n", "<leader>v", ":vertical resize +5<cr>")
+utils.map("n", "<leader>\\", "<c-w>|")
+utils.map("n", "<leader>=", "<c-w>=")
+
 -- move lines
 utils.map("v", "K", ":m '<-2<cR>gv=gv")
 utils.map("v", "J", ":m '>+1<cR>gv=gv")
-
--- modify redo
-utils.map("n", "U", "<c-r>")
 
 -- universal hjkl
 utils.map("i", "<c-h>", "<Left>")
