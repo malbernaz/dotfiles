86a1c6da687cdb008840013e98d7524af571c3f6
Miguel Albernaz <albernazmiguel@gmail.com>
2022-01-06 19:15:19 +0000

    updates

--- nvim/.config/nvim/lua/malbernaz/autopairs.lua
+++ nvim/.config/nvim/lua/malbernaz/autopairs.lua
@@ -1,27 +1,16 @@
 return function()
   local npairs = require("nvim-autopairs")
-  local utils = require("malbernaz.utils")
+  local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+  local cmp = require("cmp")
 
-  npairs.setup({})
+  npairs.setup({
+    check_ts = true,
+    ts_config = {
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
+    },
+    disable_filetype = { "TelescopePrompt" },
+  })
 
-  _G.MUtils = {}
-
-  vim.g.completion_confirm_key = ""
-
-  MUtils.completion_confirm = function()
-    if vim.fn.pumvisible() ~= 0 then
-      if vim.fn.complete_info()["selected"] ~= -1 then
-        require("completion").confirmCompletion()
-        return npairs.esc("<c-y>")
-      else
-        vim.api.nvim_select_popupmenu_item(0, false, false, {})
-        require("completion").confirmCompletion()
-        return npairs.esc("<c-n><c-y>")
-      end
-    else
-      return npairs.autopairs_cr()
-    end
-  end
-
-  utils.map("i", "<CR>", "v:lua.MUtils.completion_confirm()", { expr = true })
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 end
