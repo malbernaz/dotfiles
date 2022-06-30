6cb97ee79179f93091b6bd45d5f183c9ece8cfce
Miguel Albernaz <albernazmiguel@gmail.com>
2022-06-30 00:11:41 -0300

    updates

--- nvim/.config/nvim/lua/malbernaz/lsp/utils.lua
+++ nvim/.config/nvim/lua/malbernaz/lsp/utils.lua
@@ -13,39 +13,39 @@ map("n", "<leader>q", dgnt.setloclist)
 local M = {}
 
 function M.on_init(client)
-	client.config.flags = client.config.flags or {}
-	client.config.flags.allow_incremental_sync = true
+  client.config.flags = client.config.flags or {}
+  client.config.flags.allow_incremental_sync = true
 end
 
 function M.on_attach(_, buffn)
-	vim.o.omnifunc = "v:lua:vim.lsp.omnifunc"
+  vim.o.omnifunc = "v:lua:vim.lsp.omnifunc"
 
-	local bufopts = { buffer = buffn }
+  local bufopts = { buffer = buffn }
 
-	map("n", "K", buf.hover, bufopts)
-	map("n", "gD", buf.declaration, bufopts)
-	map("n", "gd", buf.definition, bufopts)
-	map("n", "gi", buf.implementation, bufopts)
-	map("n", "gr", buf.references, bufopts)
-	map("n", "<C-s>", buf.signature_help, bufopts)
-	map("n", "<leader>wa", buf.add_workspace_folder, bufopts)
-	map("n", "<leader>wr", buf.remove_workspace_folder, bufopts)
-	map("n", "<leader>wl", function()
-		print(vim.inspect(buf.list_workspace_folders()))
-	end, bufopts)
-	map("n", "<leader>D", buf.type_definition, bufopts)
-	map("n", "<leader>rn", buf.rename, bufopts)
-	map("n", "<leader>ca", buf.code_action, bufopts)
+  map("n", "K", buf.hover, bufopts)
+  map("n", "gD", buf.declaration, bufopts)
+  map("n", "gd", buf.definition, bufopts)
+  map("n", "gi", buf.implementation, bufopts)
+  map("n", "gr", buf.references, bufopts)
+  map("n", "<C-s>", buf.signature_help, bufopts)
+  map("n", "<leader>wa", buf.add_workspace_folder, bufopts)
+  map("n", "<leader>wr", buf.remove_workspace_folder, bufopts)
+  map("n", "<leader>wl", function()
+    print(vim.inspect(buf.list_workspace_folders()))
+  end, bufopts)
+  map("n", "<leader>D", buf.type_definition, bufopts)
+  map("n", "<leader>rn", buf.rename, bufopts)
+  map("n", "<leader>ca", buf.code_action, bufopts)
 end
 
 M.capabilities = cmp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 function M.makeConfig(config)
-	return vim.tbl_deep_extend("force", {
-		capabilities = M.capabilities,
-		on_init = M.on_init,
-		on_attach = M.on_attach,
-	}, config or {})
+  return vim.tbl_deep_extend("force", {
+    capabilities = M.capabilities,
+    on_init = M.on_init,
+    on_attach = M.on_attach,
+  }, config or {})
 end
 
 return M
