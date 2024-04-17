65b4f8cd51a5a383c7b448b04558e4cfb7159d9d
Miguel Albernaz <albernazmiguel@gmail.com>
2024-04-17 18:21:19 +0100

    adds zed

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,11 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Paste last yanked, not deleted
-vim.keymap.set("n", "`p", '"0p', { silent = true })
-vim.keymap.set("n", "`P", '"0P', { silent = true })
-vim.keymap.set("v", "P", '"_dP', { silent = true })
-
 -- Move lines on visual mode
 vim.keymap.set("v", "<s-j>", ":m '>+1<cr>gv=gv", { silent = true })
 vim.keymap.set("v", "<s-k>", ":m '<-2<cr>gv=gv", { silent = true })
