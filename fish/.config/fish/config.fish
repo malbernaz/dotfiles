0a926a68a4ab9ad099f601276ad4cd17cd3c6226
Miguel Albernaz <albernazmiguel@gmail.com>
2024-04-19 16:59:16 +0100

    cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,26 +7,17 @@ fish_add_path ~/.local/bin
 set EDITOR 'nvim'
 set GIT_EDITOR 'nvim'
 
-set NVIMRC $HOME/.dotfiles/nvim/.config/nvim/init.lua 
+set NVIMRC $HOME/.dotfiles/nvim/.config/nvim/init.lua
 set KITTY_CONFIG_DIRECTORY $HOME/.dotfiles/kitty/.config/kitty
 
 # Aliases
 alias v=nvim
 alias p=pnpm
-alias gs="git status"
-alias ga="git add"
-alias gc="git commit"
-alias gcl="git clone"
-alias gpu="git pull"
-alias gp="git push"
+alias z=zed
 function gpo
-  git push -u origin (git_branch) 
+  git push -u origin (git_branch)
 end
 
 if [ -f /opt/homebrew/bin/brew ]
   eval (/opt/homebrew/bin/brew shellenv)
 end
-
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
