f01121bbabe3763be783f1aa1ef1f51e510f3d3c
Miguel Albernaz <albernazmiguel@gmail.com>
2024-05-09 18:09:47 +0100

    updates

--- macos.sh
+++ macos.sh
@@ -3,7 +3,9 @@
 if ! command -v brew &> /dev/null; then
   echo "installing brew and basic packages..."
   command /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  command brew install stow neovim starship
+  command brew install stow neovim starship zoxide exa node zsh-completions
+  command chmod go-w '/opt/homebrew/share'
+  command chmod -R go-w '/opt/homebrew/share/zsh'
 fi
 
 command stow --ignore=\.git/ */
