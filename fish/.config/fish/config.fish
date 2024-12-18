db5ffb63c7bdb510ae2cd6bbb7d97086c7eff1ac
Miguel Albernaz <albernazmiguel@gmail.com>
2024-12-18 11:09:35 +0000

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,10 +2,11 @@
 fish_add_path ~/.yarn/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
+fish_add_path /opt/homebrew/opt/rustup/bin
 
 # Editor
-set EDITOR 'nvim'
-set GIT_EDITOR 'nvim'
+set EDITOR nvim
+set GIT_EDITOR nvim
 
 set NVIMRC $HOME/.dotfiles/nvim/.config/nvim/init.lua
 set KITTY_CONFIG_DIRECTORY $HOME/.dotfiles/kitty/.config/kitty
@@ -13,11 +14,20 @@ set KITTY_CONFIG_DIRECTORY $HOME/.dotfiles/kitty/.config/kitty
 # Aliases
 alias v=nvim
 alias p=pnpm
-alias z=zed
+alias :q=exit
 function gpo
-  git push -u origin (git_branch)
+    git push -u origin (git_branch)
 end
 
 if [ -f /opt/homebrew/bin/brew ]
-  eval (/opt/homebrew/bin/brew shellenv)
+    eval (/opt/homebrew/bin/brew shellenv)
 end
+
+zoxide init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/Users/malbernaz/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,49 +1,49 @@
 function fish_prompt --description 'Write out the prompt'
-  if [ $fish_key_bindings = fish_vi_key_bindings ]
-    set_color $fish_color_autosuggestion
-    echo -n "("
-    switch $fish_bind_mode
-      case default
+    if [ $fish_key_bindings = fish_vi_key_bindings ]
+        set_color $fish_color_autosuggestion
+        echo -n "("
+        switch $fish_bind_mode
+            case default
+                set_color green
+                echo -n n
+            case insert
+                set_color cyan
+                echo -n i
+            case visual
+                set_color magenta
+                echo -n v
+            case replace
+                set_color red
+                echo -n r
+            case replace_one
+                set_color red
+                echo -n r
+        end
+        set_color $fish_color_autosuggestion
+        echo -n ") "
+    end
+
+    set_color white
+    echo -n λ (prompt_pwd)/
+
+    set -l git_branch (command git symbolic-ref --quiet --short HEAD 2> /dev/null)
+    set -l git_commit (command git rev-parse --short HEAD 2> /dev/null)
+    set -l git_dirty (command git status -s --ignore-submodules=dirty 2> /dev/null)
+
+    if test -n "$git_branch"; or test -n "$git_commit"
         set_color green
-        echo -n "n"
-      case insert
-        set_color cyan
-        echo -n "i"
-      case visual
-        set_color magenta
-        echo -n "v"
-      case replace
-        set_color red
-        echo -n "r"
-      case replace_one
-        set_color red
-        echo -n "r"
-    end
-    set_color $fish_color_autosuggestion
-    echo -n ") "
-  end
 
-  set_color white
-  echo -n "λ" (prompt_pwd)/
+        if test -n "$git_branch"
+            echo -n " $git_branch"
+        else
+            echo -n " $git_commit"
+        end
 
-  set -l git_branch (command git symbolic-ref --quiet --short HEAD 2> /dev/null)
-  set -l git_commit (command git rev-parse --short HEAD 2> /dev/null)
-  set -l git_dirty (command git status -s --ignore-submodules=dirty 2> /dev/null)
-
-  if test -n "$git_branch"; or test -n "$git_commit"
-    set_color green
-
-    if test -n "$git_branch"
-      echo -n " $git_branch"
-    else
-      echo -n " $git_commit"
+        if test -n "$git_dirty"
+            echo -n "*"
+        end
     end
 
-    if test -n "$git_dirty"
-      echo -n "*"
-    end
-  end
-
-  set_color white
-  echo -n " "
+    set_color white
+    echo -n " "
 end
