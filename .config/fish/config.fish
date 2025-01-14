022083279b5a1d8358e48a58fea00427e1ff2815
Miguel Albernaz <albernazmiguel@gmail.com>
2025-01-14 12:08:35 -0300

    better fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,8 @@ fish_add_path ~/.local/bin
 fish_add_path ~/Library/pnpm
 
 # Variables
-set EDITOR nvim
-set GIT_EDITOR nvim
+set EDITOR hx
+set GIT_EDITOR hx
 set NVIMRC $HOME/.dotfiles/.config/nvim/init.lua
 set KITTY_CONFIG_DIRECTORY $HOME/.dotfiles/.config/kitty
 
@@ -18,11 +18,22 @@ alias p=pnpm
 alias :q=exit
 alias gpo="git push -u origin $(git_branch)"
 
+# yazi
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 # Homebrew
 if [ -f /opt/homebrew/bin/brew ]
     eval (/opt/homebrew/bin/brew shellenv)
 end
 set -gx HOMEBREW_AUTO_UPDATE_SECS 604800 # one week
+set -gx HOMEBREW_NO_ENV_HINTS false # disable hints
 
 # Zoxide
 zoxide init --cmd cd fish | source
